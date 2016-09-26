04cb71e8e81fae2a214eeb18bfec970e57a6b165
Edwin Martin <edwin@bitstorm.org>
2016-09-26 22:44:12 +0200

    Now accepting url and creating dynamic port

--- testproxy.js
+++ testproxy.js
@@ -1,5 +1,5 @@
 /**
- testProxy 1.0.0
+ testProxy 1.1.0
  Test your local websites on other devices
  @copyright 2016 Edwin Martin
  @see {@link http://www.bitstorm.org/javascript/}
@@ -9,11 +9,14 @@
 const http = require('http');
 const httpProxy = require('http-proxy');
 
-const args = getArguments();
-if (args) {
-    startProxy(args.hostname, args.port)
-} else {
-    console.log("Usage: node testproxy.js <domainname> <port>");
+try {
+	const args = getArguments();
+	startProxy(args.host, args.port, args.listenPort)
+} catch(e) {
+	console.log('e', e);
+	console.error("Oops. " + e.message);
+	console.error("Usage: node testproxy.js <url>");
+	console.error("or     node testproxy.js <url> <listen-port>");
 }
 
 //=== Function declarations ============
@@ -22,13 +25,41 @@ if (args) {
  * Parse command line arguments
  */
 function getArguments() {
+	var host, port, listenPort;
+
     if (process.argv.length < 3) {
-        return null;
+	    throw {message: "testProxy does not see enough arguments."};
     }
+
+	({host, port} = parseHost(process.argv[2]));
+	listenPort = process.argv[3] || 0;
+
+
     return {
-        hostname: process.argv[2],
-        port: process.argv[3] || 9000
+        host,
+        port,
+	    listenPort
     };
+}
+
+function parseHost(host) {
+	var match = host.match(/http(s?):\/\/([-a-z0-9\.]+):?([0-9]+)?\/?/);
+
+	if (!match || !match[2]) {
+		throw {message: "testProxy does not inderstand the URL."};
+	}
+
+	if (match[1] == 's') {
+		throw {message: "testProxy does not work with https."};
+	}
+
+	var host = match[2];
+	var port = match[3] || 80;
+
+	return {
+		host,
+		port
+	};
 }
 
 /**
@@ -46,7 +77,7 @@ function getIpAddresses() {
     , []);
 }
 
-function startProxy(hostname, port) {
+function startProxy(hostname, port, listenPort) {
     const proxy = httpProxy.createProxyServer({});
 
     proxy.on('proxyReq', proxyReq =>
@@ -55,13 +86,27 @@ function startProxy(hostname, port) {
 
     const server = http.createServer((req, res) =>
         proxy.web(req, res, {
-            target: `http://${hostname}`
+            target: `http://${hostname}:${port}`
         })
     );
 
-    server.listen(port);
+	server.listen(listenPort);
+
+	listenPort = server.address().port;
 
     getIpAddresses().forEach(ip =>
-        console.log(`Listening on http://${ip.address}:${port}`)
+        console.log(`Listening on http://${ip.address}:${listenPort}`)
     );
 }
+
+
+/**
+ * Show QR code on the terminal
+ * @param url
+ */
+function showQR(url) {
+    const qrcode = require('qrcode-terminal');
+
+    qrcode.generate(url);
+}
+
